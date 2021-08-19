359b1d678a851759428e47ff60bb6f272f530746
ernirohmawati <89213152+ernirohmawati@users.noreply.github.com>
2021-08-19 23:38:27 +0700

    Add files via upload

---
+++ Latihan3.php
@@ -0,0 +1,25 @@
+<?php
+    include 'koneksi.php';
+    
+    $db     = new Database();
+    $con    = $db->Connect();
+    $id     = $_GET['id'];
+    $data   = mysqli_query($con,"SELECT * FROM tbl_mahasiswa WHERE id='$id'");
+    $d = mysqli_fetch_array($data);   
+
+    if(isset($_POST['proses']))
+    {
+        mysqli_query($con, "UPDATE tbl_mahasiswa  SET nama = '$_POST[nama]',npm = '$_POST[npm]'
+        WHERE id = '$_GET[id]'");
+        echo "<script>alert('Data Berhasil Diupdate')('location:latihan1.php');</script>";
+    }
+?>
+
+<h1>Tambah Mahasiswa</h2>
+<a href="latihan1.php"><span><= Kembali</span></a>
+<br></br>
+<form action="" method="POST">
+    <input type="text" name="nama" value="<?php echo $d['nama'];?>">
+    <input type="text" name="npm" value="<?php echo $d['npm'];?>">
+    <input type="submit" name="proses" value="Update">
+</form>
